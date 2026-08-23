5832bd182e23b8d7608dcb15dfc91089ed24b347
Laiba nurr <laibasirahh7@gmail.com>
2026-08-23 15:40:17 +0500

    feat: add CartItem model to manage cart item data

---
+++ lib/src/cubit/data/models/cart_item.dart
@@ -0,0 +1,24 @@
+class CartItem {
+  final String id;
+  final String name;
+  final double price;
+  final int quantity;
+
+  CartItem({
+    required this.id,
+    required this.name,
+    required this.price,
+    this.quantity = 1,
+  });
+
+  CartItem copyWith({int? quantity}) {
+    return CartItem(
+      id: id,
+      name: name,
+      price: price,
+      quantity: quantity ?? this.quantity,
+    );
+  }
+
+  double get total => price * quantity;
+}
