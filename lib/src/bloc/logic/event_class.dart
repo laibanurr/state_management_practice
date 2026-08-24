43a267b263fd158a1aedf4c776770350deae80c8
Laiba nurr <laibasirahh7@gmail.com>
2026-08-24 21:38:29 +0500

    feat: add event classes for liking and disliking jokes

---
+++ lib/src/bloc/logic/event_class.dart
@@ -0,0 +1,13 @@
+import 'package:state_management/src/bloc/data/models/joke_data.dart';
+
+abstract class JokeEvent {}
+
+class AddToLikesEvent extends JokeEvent {
+  final JokeData jokeData;
+  AddToLikesEvent(this.jokeData);
+}
+
+class DislikeTheJokeEvent extends JokeEvent {
+  final JokeData jokeData;
+  DislikeTheJokeEvent(this.jokeData);
+}
