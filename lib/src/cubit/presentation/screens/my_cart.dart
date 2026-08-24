26f451db6f4606b0cb7a53931167bc08906e7308
Laiba nurr <laibasirahh7@gmail.com>
2026-08-24 19:33:03 +0500

    feat: update home screen to use CartScreenCubit and replace BlocConsumerScreen with MyCartConsumerScreen

--- lib/src/cubit/presentation/screens/my_cart.dart
+++ lib/src/cubit/presentation/screens/my_cart.dart
@@ -1,62 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:state_management/src/cubit/logic/cart_cubit.dart';
-import 'package:state_management/src/cubit/logic/cart_state.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:state_management/src/cubit/logic/cart_cubit.dart';
+// import 'package:state_management/src/cubit/logic/cart_state.dart';
 
-class MyCart extends StatelessWidget {
-  const MyCart({super.key});
+// class MyCart extends StatelessWidget {
+//   const MyCart({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurpleAccent,
-        title: Text(
-          'HAHAHAHAH MYYY CARTTTTTT 🙏🙄😭😭',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-      ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.deepPurpleAccent,
+//         title: Text(
+//           'HAHAHAHAH MYYY CARTTTTTT 🙏🙄😭😭',
+//           style: TextStyle(color: Colors.white, fontSize: 24),
+//         ),
+//       ),
 
-      body: BlocBuilder<CartCubit, CartState>(
-        builder: (context, cartState) {
-          if (cartState.items.isEmpty) {
-            return const Center(child: Text('Your cart is empty!'));
-          }
-          return Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: cartState.items.length,
-                  itemBuilder: (context, index) {
-                    final myItem = cartState.items[index];
-                    return ListTile(
-                      leading: Text(myItem.name),
-                      subtitle: Text('Qty: ${myItem.quantity}'),
+//       body: BlocBuilder<CartCubit, CartState>(
+//         builder: (context, cartState) {
+//           if (cartState.items.isEmpty) {
+//             return const Center(child: Text('Your cart is empty!'));
+//           }
+//           return Column(
+//             children: [
+//               Expanded(
+//                 child: ListView.builder(
+//                   itemCount: cartState.items.length,
+//                   itemBuilder: (context, index) {
+//                     final myItem = cartState.items[index];
+//                     return ListTile(
+//                       leading: Text(myItem.name),
+//                       subtitle: Text('Qty: ${myItem.quantity}'),
 
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            '\$${myItem.total.toStringAsFixed(2)}',
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(width: 8),
-                          IconButton(
-                            onPressed: () {
-                              context.read<CartCubit>().removeItem(myItem.id);
-                            },
-                            icon: const Icon(Icons.delete_forever),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-}
+//                       trailing: Row(
+//                         mainAxisSize: MainAxisSize.min,
+//                         children: [
+//                           Text(
+//                             '\$${myItem.total.toStringAsFixed(2)}',
+//                             style: const TextStyle(fontWeight: FontWeight.bold),
+//                           ),
+//                           const SizedBox(width: 8),
+//                           IconButton(
+//                             onPressed: () {
+//                               context.read<CartCubit>().removeItem(myItem.id);
+//                             },
+//                             icon: const Icon(Icons.delete_forever),
+//                           )
+//                         ],
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               ),
+//             ],
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
