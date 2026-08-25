bdd98aafa5419c7755bacda76c3865a5bb7b33fc
Laiba nurr <laibasirahh7@gmail.com>
2026-08-25 15:27:58 +0500

    feat: enhance JokeState with currentJokeIndex and add JokeScreenWithContainer for joke display and navigation

--- lib/src/bloc/logic/joke_state.dart
+++ lib/src/bloc/logic/joke_state.dart
@@ -2,8 +2,12 @@ import 'package:state_management/src/bloc/data/models/joke_data.dart';
 
 class JokeState {
   final List<JokeData> jokeData;
-  JokeState({this.jokeData = const []});
-  JokeState copyWith({List<JokeData>? jokeData}) {
-    return JokeState(jokeData: jokeData ?? this.jokeData);
+  final int  currentJokeIndex;
+  JokeState({this.jokeData = const [],
+  this.currentJokeIndex=0});
+  JokeState copyWith({List<JokeData>? jokeData , int? currentJokeIndex}) {
+    return JokeState(jokeData: jokeData ?? this.jokeData,
+    currentJokeIndex: currentJokeIndex?? this.currentJokeIndex,
+     );
   }
 }
